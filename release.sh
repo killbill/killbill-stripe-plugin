5b71889f646ab516b2df6c490f3eca3fa53e8b82
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-03-12 12:42:50 -0400

    pom.xml: change groupId to org.kill-bill.billing.plugin.ruby
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- release.sh
+++ release.sh
@@ -1,5 +1,18 @@
 set -e
 
+if [ "GNU" != "$(tar --help | grep GNU | head -1 | awk '{print $1}')" ]; then
+  echo "Unable to release: make sure to use GNU tar"
+  exit 1
+fi
+
+if $(ruby -e'require "java"'); then
+  # Good
+  echo "Detected JRuby"
+else
+  echo "Unable to release: make sure to use JRuby"
+  exit 1
+fi
+
 VERSION=`grep -E '<version>([0-9]+\.[0-9]+\.[0-9]+)</version>' pom.xml | sed 's/[\t \n]*<version>\(.*\)<\/version>[\t \n]*/\1/'`
 if [ "$VERSION" != "$(cat $PWD/VERSION)" ]; then
   echo "Unable to release: make sure the versions in pom.xml and VERSION match"
@@ -18,7 +31,7 @@ rake killbill:package
 ARTIFACT="$PWD/pkg/killbill-stripe-$VERSION.tar.gz"
 echo "Pushing $ARTIFACT to Maven Central"
 mvn gpg:sign-and-deploy-file \
-    -DgroupId=com.ning.killbill.ruby \
+    -DgroupId=org.kill-bill.billing.plugin.ruby \
     -DartifactId=stripe-plugin \
     -Dversion=$VERSION \
     -Dpackaging=tar.gz \
