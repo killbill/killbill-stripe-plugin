1c73977085aa6a95f24a8e05a98d80232ac758da
Andrew Ritchie <hello@andrewritchie.info>
2017-12-12 18:50:10 +0100

    Move bank account class

--- lib/stripe/bank_account.rb
+++ lib/stripe/bank_account.rb
@@ -1,13 +1,4 @@
 module Killbill
   module Stripe
-    class BankAccount
-      attr_accessor :bank_name, :account_number, :routing_number, :type
-
-      def initialize(args)
-        args.each do |k,v|
-          instance_variable_set("@#{k}", v) unless v.nil?
-        end
-      end
-    end
   end
 end
